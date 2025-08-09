42f5aa69ed7f245b7efdfe0ed34fc32d56f13ff2
AmanAfreedi <amanpasha660@gmail.com>
2025-08-09 08:57:17 +0530

    fixed bugs

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,7 @@ const Product = (info) => {
     
 
       <Link to={`/productDetails/${info.id}`} className='font-semibold'>
-      <div className='w-[120px] overflow-hidden h-[450px] md:max-w-[380px] md:h-auto w-auto m-3  flex flex-col p-2'>
+      <div className=' overflow-hidden h-[450px] -mb-18  md:max-w-[380px] md:h-auto w-auto md:my-3 mx-1 flex flex-col p-2'>
         <img className='aspect-square md:object-contain border-[0.1px] border-gray-300' src={info.img} alt="" />
         <p className='text-gray-400 text-xs'>{info.category}</p>
         <h1 className='font-bold'>{info.title}</h1>
